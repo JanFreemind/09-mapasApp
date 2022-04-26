9e6ac6c1e74b60c48ae960bee5c9033fafd3630d
JanPLosa <jplosa@tlsi.es>
2022-04-26 17:05:44 +0200

    Mostrar un mapa en pantalla completa

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1IjoiamFucGxvc2EiLCJhIjoiY2wyZzhibWp3MDEwOTNlbXdiejZ1aWpmdCJ9.8ICZOWaLXZS7LuDyYqsI6g'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mapboxToken: 'pk.eyJ1IjoiamFucGxvc2EiLCJhIjoiY2wyZzhibWp3MDEwOTNlbXdiejZ1aWpmdCJ9.8ICZOWaLXZS7LuDyYqsI6g'
 };
 
 /*
