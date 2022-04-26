7f22e41a51094fa4eda2d47ad4e16b574c9e441c
JanPLosa <jplosa@tlsi.es>
2022-04-26 17:30:10 +0200

    Pagina zoom-range diseño

--- src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
-  styles: [
+  styles: [`
+    .mapa-container {
+      height: 100%;
+      width: 100%;
+    }
+
+    .row {
+      background-color: white;
+      border.radius: 5px;
+      bottom: 50px;
+      left: 50px;
+      padding: 10px;
+      position: fixed;
+      z-index: 999;
+    }
+  `
   ]
 })
 export class ZoomRangeComponent implements OnInit {
@@ -11,6 +27,13 @@ export class ZoomRangeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    var map = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [ -0.355643341466306, 39.47783918180663 ],
+      zoom: 16
+    });
   }
 
 }
